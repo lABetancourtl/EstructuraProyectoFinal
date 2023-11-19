7958dad2d71f938489e346bf40ec0641f8da7c0f
lABetancourtl <anderson.betancourta@uqvirtual.edu.co>
2023-11-19 11:38:12 -0500

    Tale tareas funcionando crear y eliminar

--- src/main/java/com/example/demo/Modelo/Usuario.java
+++ src/main/java/com/example/demo/Modelo/Usuario.java
@@ -3,7 +3,6 @@ package com.example.demo.Modelo;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.SplittableRandom;
 
 public class Usuario implements Serializable {
     private static final long serialVersionUID = 1L;
